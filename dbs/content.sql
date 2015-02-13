8ac048fc73adefad8570034607caf75c04f468c7
Losses Don <losses.don@gmail.com>
2015-02-13 18:05:40 +0800

    数据库更新

--- dbs/content.sql
+++ dbs/content.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 13, 2015 at 06:53 AM
+-- Generation Time: Feb 13, 2015 at 11:01 AM
 -- Server version: 5.6.21
 -- PHP Version: 5.6.3
 
@@ -29,13 +29,13 @@ SET time_zone = "+00:00";
 CREATE TABLE IF NOT EXISTS `content` (
 `id` int(11) NOT NULL,
   `author` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `title` text COLLATE utf8_unicode_ci NOT NULL,
+  `title` text COLLATE utf8_unicode_ci,
   `content` text COLLATE utf8_unicode_ci NOT NULL,
   `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `active_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `img` tinytext COLLATE utf8_unicode_ci,
   `upid` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Indexes for dumped tables
@@ -55,7 +55,7 @@ ALTER TABLE `content`
 -- AUTO_INCREMENT for table `content`
 --
 ALTER TABLE `content`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

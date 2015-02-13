4ae0e56b580628d1c7a8697536228d9974aceae6
Losses Don <losses.don@gmail.com>
2015-02-13 18:07:17 +0800

    脚本分离

--- scripts/board.js
+++ scripts/board.js
@@ -10,30 +10,3 @@ mKnowledge.controller('getPostCtrl', function ($http, $scope) {
             $scope.posts = response;
         });
 });
-
-$(document).ready(function () {
-    $('#new_post').click(function () {
-        $('#post_dialog').addClass('flow_up')
-            .removeClass('flow_down')
-            .on('click.activeForm', function () {
-                return false;
-            });
-
-        $('#new_post').addClass('hide')
-            .removeClass('show');
-
-        setTimeout(function () {
-            $('body').on('click.activeBody', function () {
-                $('#post_dialog').addClass('flow_down')
-                    .removeClass('flow_up')
-                    .off('click.activeForm');
-
-                $(this).off('click.activeBody');
-
-                $('#new_post').removeClass('hide')
-                    .addClass('show');
-            });
-        }, 100);
-
-    });
-});

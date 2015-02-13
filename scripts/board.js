fb6c8bc0da5570db4a6e960a4abcd589ea21aa92
Losses Don <losses.don@gmail.com>
2015-02-13 21:48:47 +0800

    滚动更新

--- scripts/board.js
+++ scripts/board.js
@@ -5,8 +5,34 @@ var mKnowledge = angular.module('mKnowledge', []);
 
 mKnowledge.controller('getPostCtrl', function ($http, $scope) {
 
-    $http.get("api/?list")
-        .success(function (response) {
-            $scope.posts = response;
-        });
+    var page = 1
+        , loading = false;
+
+    $scope.posts = [];
+
+    function pushContent() {
+        if (!loading) {
+            loading = true;
+            $http.get("api/?list&page=" + page)
+                .success(function (response) {
+                    for (var i = 0; i <= response.length - 1; i++) {
+                        console.log(i);
+                        console.log(response[i]);
+
+                        $scope.posts.push(response[i]);
+                    }
+                    loading = false;
+                });
+
+            page++;
+        }
+    }
+
+    $(window).on('scroll', function (event) {
+        if ($(document).scrollTop() + $(window).height() >= $(document).height()) {
+            pushContent();
+        }
+    });
+
+    pushContent();
 });

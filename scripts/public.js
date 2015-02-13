87d373a4e041e58918128f22ec4c63ae6b50c798
Losses Don <losses.don@gmail.com>
2015-02-13 18:06:55 +0800

    脚本分离

---
+++ scripts/public.js
@@ -0,0 +1,30 @@
+/**
+ * Created by Don on 2/13/2015.
+ */
+$(document).ready(function () {
+    $('#new_post').click(function () {
+        $('#post_dialog').addClass('flow_up')
+            .removeClass('flow_down')
+            .on('click.activeForm', function (event) {
+                if (!$(event.target).hasClass('submit'))
+                    return false;
+            });
+
+        $('#new_post').addClass('hide')
+            .removeClass('show');
+
+        setTimeout(function () {
+            $('body').on('click.activeBody', function () {
+                $('#post_dialog').addClass('flow_down')
+                    .removeClass('flow_up')
+                    .off('click.activeForm');
+
+                $(this).off('click.activeBody');
+
+                $('#new_post').removeClass('hide')
+                    .addClass('show');
+            });
+        }, 100);
+
+    });
+});

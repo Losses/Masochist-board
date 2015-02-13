592fb1c37ee8ba8624b9fe0c73fab254002e7c42
Losses Don <losses.don@gmail.com>
2015-02-13 15:58:16 +0800

    增加页面弹出动画

--- scripts/board.js
+++ scripts/board.js
@@ -1,9 +1,36 @@
 var mKnowledge = angular.module('mKnowledge', []);
 
-mKnowledge.controller('getPostCtrl', function($http, $scope) {
+mKnowledge.controller('getPostCtrl', function ($http, $scope) {
 
-	$http.get("api/?list")
-		 .success(function(response) {
-			$scope.posts = response;
-		 });
+    $http.get("api/?list")
+        .success(function (response) {
+            $scope.posts = response;
+        });
+});
+
+$(document).ready(function () {
+    $('#new_post').click(function () {
+        $('#post_dialog').addClass('flow_up')
+            .removeClass('flow_down')
+            .on('click.activeForm', function () {
+                return false;
+            });
+
+        $('#new_post').addClass('hide')
+            .removeClass('show');
+
+        setTimeout(function () {
+            $('body').on('click.activeBody', function () {
+                $('#post_dialog').addClass('flow_down')
+                    .removeClass('flow_up')
+                    .off('click.activeForm');
+
+                $(this).off('click.activeBody');
+
+                $('#new_post').removeClass('hide')
+                    .addClass('show');
+            });
+        }, 100);
+
+    });
 });

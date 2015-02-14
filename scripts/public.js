e06a4bd3b048a13c40af4a0ced08e1999213f202
Losses Don <losses.don@gmail.com>
2015-02-14 10:23:57 +0800

    前端增加页内分页

--- scripts/public.js
+++ scripts/public.js
@@ -1,6 +1,55 @@
 /**
  * Created by Don on 2/13/2015.
  */
+
+var mKnowledge = angular.module('mKnowledge', []);
+
+mKnowledge.controller('getPostCtrl', function ($http, $scope) {
+
+    var page = 1
+        , loading = false;
+
+    $scope.posts = [];
+
+    function pushContent() {
+        if (!inPost) {
+            if (!loading) {
+                loading = true;
+                $http.get("api/?list&page=" + page)
+                    .success(function (response) {
+                        for (var i = 0; i <= response.length - 1; i++) {
+                            $scope.posts.push(response[i]);
+                        }
+                        loading = false;
+                    });
+            }
+        } else {
+            if (!loading) {
+                loading = true;
+                var hash = window.location.hash.split('#')[1];
+                $http.get("api/?post&id=" + hash + "&page=" + page)
+                    .success(function (response) {
+                        console.log(response);
+                        for (var i = 0; i <= response.length - 1; i++) {
+                            $scope.posts.push(response[i]);
+                        }
+                        loading = false;
+                    });
+            }
+        }
+
+        page++;
+    }
+
+    $(window).on('scroll', function () {
+        if ($(document).scrollTop() + $(window).height() >= $(document).height()) {
+            pushContent();
+        }
+    });
+
+    pushContent();
+});
+
 $(document).ready(function () {
     $('#new_post').click(function () {
         $('#post_dialog').addClass('flow_up')
@@ -30,4 +79,78 @@ $(document).ready(function () {
         }, 100);
 
     });
+
+    var losses = {
+        elements: {
+            submitable: false,
+            titleElement: !inPost ? $('input[name="title"]') : null,
+            contentElement: $('textarea[name="content"]'),
+            submitIcon: $('input[type="submit"]')
+        }
+    };
+
+    function checkSumitable() {
+        setTimeout(function () {
+            losses.elements.submitable = inPost ? (
+            (losses.elements.contentElement.val().length <= 35)
+            && (losses.elements.contentElement.val().length !== 0)
+            ) : (
+            (losses.elements.titleElement.val().length <= 35)
+            && (losses.elements.titleElement.val().length !== 0)
+            && (losses.elements.contentElement.val().length <= 35)
+            && (losses.elements.contentElement.val().length !== 0)
+            );
+
+            if (!losses.elements.submitable) {
+                losses.elements.submitIcon.addClass('lock');
+            }
+            else {
+                losses.elements.submitIcon.removeClass('lock');
+            }
+        }, 10);
+    }
+
+    if (!inPost) {
+        losses.elements.titleElement.keypress(checkSumitable);
+        losses.elements.titleElement.change(checkSumitable);
+    }
+
+    losses.elements.contentElement.keypress(checkSumitable);
+    losses.elements.contentElement.change(checkSumitable);
+
+    $('form').submit(function (event) {
+        console.log(!losses.elements.submitable);
+        if (!losses.elements.submitable)
+            return;
+
+        var flying = true;
+        var postContent = {
+            'author': $('input[name="author"]').val(),
+            'title': !inPost ? $('input[name="title"]').val() : null,
+            'content': $('textarea[name="content"]').val(),
+            'upid': $('input[name="upid"]').val()
+        };
+
+        losses.elements.submitIcon.blur()
+            .addClass('fly');
+
+        setTimeout(function () {
+            flying = false;
+        }, 500);
+
+        $.post('api/?new', postContent, function (data) {
+            if (inPost)
+                location.reload(true);
+            if (data == 'false')
+                return;
+            var intervalItem = setInterval(function () {
+                if (!flying)
+                    window.location.href = 'post.html#' + data;
+            }, 100);
+        });
+
+        event.preventDefault();
+    });
+
+    checkSumitable();
 });

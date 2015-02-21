71db156b6b49f4088bd46bf7580661e43b5b9b8f
sky <midasfree@gmail.com>
2015-02-21 23:44:01 +0800

    后端代码重构完毕

--- api/index.php
+++ api/index.php
@@ -16,7 +16,7 @@
     'username'    =>  DB_USER,
     'password'    =>  DB_PASSWORD,
     'port'      =>  DB_PORT,
-    'charest'   =>  'utf8',
+    'charset'   =>  'utf8',
     'option'    =>  [PDO::ATTR_CASE =>  PDO::CASE_NATURAL]
   
   ]);
@@ -29,11 +29,12 @@
 
   if (isset($_GET['new'])) {
     
-    $post_title   = isset($_POST['title'])    ? $_POST['title']   : '';
-    $post_content = isset($_POST['content'])  ? $_POST['content'] : '';
-    $post_upid    = isset($_POST['upid'])   ? $_POST['upid']    : 0;
-    $post_sage    = isset($_POST['sage'])   ? 1         : 0;
-    $post_cate    = isset($_POST['category']) ? $_POST['category']  : 0;
+    $post_title   = isset($_POST['title'])        ? $_POST['title']   : '';
+    $post_content = isset($_POST['content'])      ? $_POST['content'] : '';
+    $post_upid    = isset($_POST['upid'])       ? $_POST['upid']    : 0;
+    $post_sage    = isset($_POST['sage'])       ? 1         : 0;
+    $post_cate    = isset($_POST['category'])     ? $_POST['category']  : 0;
+    $post_author  = isset($_SESSIOM['logined'] == true) ? '猴子'  : $_POST['author'];
 
     if ($post_upid == 0) {
 
@@ -77,12 +78,12 @@
     if ($_FILES > 0) {
 
         if ((($_FILES['image']['type'] == 'image/gif')
-          || ($_FILES['image']['type'] == 'image/jpeg')
-              || ($_FILES['image']['type'] == 'image/svg')
-              || ($_FILES['image']['type'] == 'image/bmp')
-              || ($_FILES['image']['type'] == 'image/wbmp')
-              || ($_FILES['image']['type'] == 'image/png'))
-              && ($_FILES['image']['size'] < 50000000)) {
+        || ($_FILES['image']['type'] == 'image/jpeg')
+          || ($_FILES['image']['type'] == 'image/svg')
+          || ($_FILES['image']['type'] == 'image/bmp')
+          || ($_FILES['image']['type'] == 'image/wbmp')
+          || ($_FILES['image']['type'] == 'image/png'))
+          && ($_FILES['image']['size'] < 50000000)) {
 
               if ($_FILES['image']['error']) {
 
@@ -95,30 +96,30 @@
                 
                 $type_img   = explode('.', '../upload/'
                   .$_FILES['image']['name']);
-                $rename_img = md5(md5_file('../upload/'
+                $post_img = md5(md5_file('../upload/'
                   .$_FILES['image']['name'])
                   .date('Y-m-d H:i:s')).'.'
                   .$type_img[count ($type_img) - 1];
                 rename('../upload/' . $_FILES['image']['name'],
-                  '../upload/'  . $rename_img);
+                     '../upload/' . $post_img);
                 
                 }
             
           }
     }else {
 
-        $rename_img = null;
+        $post_img = NULL;
         
     }
 
     $data_sql += [
 
-      'author'    =>  $_POST['author'],
+      'author'    =>  $post_author,
         'title'     =>  $post_title,
         'content'   =>  htmlspecialchars($post_content),
         'time'      =>  $current_time,
       'active_time' =>  $current_time,
-      'img'     =>  $rename_img,
+      'img'     =>  $post_img,
         'upid'      =>  $post_upid,
         'sage'      =>  $post_sage,
         'category'    =>  $post_cate
@@ -164,7 +165,7 @@
     ];
     $where_sql = [
 
-      'AND' =>  ['upid[=]'  =>  0],
+      'AND'   =>  ['upid[=]'  =>  0],
       'ORDER' =>  ['active_time DESC', 'time DESC'],
       'LIMIT' =>  [($_GET['page'] - 1) * 10, $_GET['page'] * 10],
 
@@ -186,164 +187,248 @@
 
   }
 
-elseif (isset($_GET['category'])) {
-  $data = $database->select('category', '*');
+  elseif (isset($_GET('category'))) {
+    
+    $data = $database->select('category', '*');
 
-  echo json_encode($data);
-}
+    echo json_encode($data);
 
-elseif (isset ($_GET['post'])) {
-  require_once('../libs/parsedown.php');
-
-  $Parsedown = new Parsedown();
-
-  $_GET['page'] = isset($_GET['page']) ? $_GET['page'] : 1;
-
-  $data = $database->select('content', [
-    'id',
-    'title',
-    'content',
-    'author',
-    'time',
-    'img',
-	'category'
-    ],[
-      'OR'        =>  [
-        'upid[=]' =>  $_GET['id'],
-        'id[=]'   =>  $_GET['id']
-        ],
-      'ORDER'     =>  ['upid','id'],
-      'LIMIT'     =>  [($_GET['page'] - 1) * 10, $_GET['page'] * 10]
-  ]);
-
-  if (isset ($data[0]['upid']) && $data[0]['upid'] != 0) {
-    response_message(301, $data[0]['upid']);
   }
 
-  $data_length = count($data);
+  elseif (isset($_GET['post'])) {
+    
+    require_once ('../libs/parsedown.php');
 
-  for ($i = 0; $i < $data_length; $i++) {
-    $data[$i]['content'] = $emotion->phrase($Parsedown->text($data[$i]['content']));
+    $Parsedown = new Parsedown();
+
+    $columns_sql = [
+
+      'id',
+      'title',
+      'content',
+      'author',
+      'time',
+      'img',
+      'category'
+
+    ];
+    $where_sql = [
+
+      'OR'  =>  [
+
+        'upid[=]' =>$_GET['id'],
+        'id[=]'   =>$_GET['id']
+
+      ],
+      'ORDER' =>  ['upid','id'],
+      'LIMIT' =>  [($_GET['page'] -1) * 10, $_GET['page'] * 10]
+
+    ];
+    $data = $database->select('content', $columns_sql, $where_sql);
+
+    if (isset($data[0]['upid']) && $data[0]['upid'] != 0) {
+      
+      response_message(301, $data[0]['upid']);
+
+    }
+
+    $data_length = count($data);
+    for ($i=0; $i < $data_length; $i++) { 
+      
+      $data[$i]['content'] =
+        $emotion->phrase($Parsedown->text($data[$i]['content']));
+
+    }
+
+    echo json_encode($data);
+
+    exit();
+
   }
 
-  echo json_encode($data);
+  elseif (isset($_GET['manage'])) {
+    
+    session_start();
 
-  exit();
-}
+    if (isset($_POST['key'])) {
+      
+      $_SESSIOM['key'] = md5(md5(date('Y-m-d H:i:s')) . UR_SALT);
 
-elseif (isset($_GET['manage'])) {
-  
-  //session check
-  session_start();
+      response_message(200, $_SESSIOM['key']);
 
-  if (isset($_POST['key'])) {
-    $_SESSION['key'] = md5(md5(date('Y-m-d H:i:s')) . UR_SALT);
-    response_message(200, $_SESSION['key']);
-  }elseif (isset($_POST['password'])) {
-    if (!isset($_SESSION['key'])) {
-      response_message(403,'You need a key!');
-    }else {
-      if(md5(md5(UR_PASSWORD) . $_SESSION['key'])  ==  $_POST['password']) {
-        $_SESSION['logined'] = true;
-        response_message(200, 'Login success!');
+    }elseif (isset($_POST['password'])) { 
+
+      if (!isset($_SESSIOM['key'])) {
+        
+        response_message(403, 'You need a key!');
+
       }else {
-        $_SESSION['logined'] = false;
-        response_message(403, 'Wrong password!');
+
+        if (md5(md5(UR_PASSWORD) . $_SESSIOM['key']) == $_POST['password']) {
+          
+          $_SESSIOM['logined'] = true;
+
+          response_message(200, '兽人永不为奴!');
+
+        }else {
+
+          $_SESSIOM['logined'] = false;
+
+          response_message(403, 'UCCU输错密码的样子，真是丑陋!');
+
+        }
+
       }
-    }
-  }elseif (isset($_POST['check'])) {
-    if (isset($_SESSION['logined']) && $_SESSION['logined'] == true) {
-      response_message(200, true);
-    }else {
-      response_message(200, false);
-    }
-  }
-  
-  //function
-  if (isset($_POST['action']) && ($_POST['action'] == 'delete')) {
-    foreach ($_POST['target'] as $post_target) {
-      $post_target_id = $database->select('content', [
-        'upid'
-      ],[
-        'id[=]'  =>  $post_target
-      ]);
-      if (isset($post_target_id) && $post_target_id != 0) {
-        $data = $database->delete('content', [
-          'upid[=]' =>  $post_target_id
-        ]);
+
+    }elseif (isset($_POST['check'])) {
+      
+      if (isset($_SESSIOM['logined']) && ($_SESSIOM['logined'] == true)) {
+        
+        response_message(200, true);
+
       }else {
-        $data = $database->delete('content', [
-          'AND' =>  [
-            'upid'  =>  $post_target_id,
-            'id'    =>  $post_target_idk
-        ]]);
+
+        response_message(403, false);
+      
       }
+
+    }
+
+    if (isset($_POST['action']) && ($_POST['action'] == 'delete')) {
+
+      $data_false = [];
+
+      foreach ($_POST['target'] as $post_target) {
+
+        $columns_sql = ['upid'];
+        $where_sql   = ['id[=]' =>  $post_target];
+        $post_target_id = $database->select('content', $columns_sql, $where_sql);
+
+        if (isset($post_target_id) && ($post_target_id != 0)) {
+
+            $data_sql = ['upid[=]'  =>  $post_target_id];
+            $data = $database->delete('content', $data_sql);
+
+        }else {
+
+            $data_sql = ['AND'  =>  [
+
+              'upid'  =>  $post_target_id,
+              'id'  =>  $post_target_id
+
+            ]];
+          $data = $database->delete('content', $data_sql);
+
+        }
+
+        if ($data == false) {
+
+            $data_false += $post_target_id;
+
+        }
+      }
+
+      if (count($data_false) == 0) {
+        
+        response_message(200, 'rm -rf /');
+
+      }else {
+
+        response_message(403, 'Delete failed OAQ ' . implode(' ', $data_false));
+
+      }
+
+    }
+
+    if (isset($_POST['action']) && ($_POST['action'] == 'sage')) {
+
+      $data_false = [];
+
+      foreach ($_POST['target'] as $post_target_id) {
+        
+        $data_sql  = ['sage'  =>  1];
+        $where_sql = ['id[=]' =>  $post_target_id];
+        $data = $database->update('content', $data_sql, $where_sql);
+
+        if ($date == false) {
+            
+            $data_false += $post_target_id;
+        
+        }
+    
+      }
+
+      if (count($data_false) == 0) {
+        
+        response_message(200, '基本法!');
+
+      }else {
+
+        response_message(403, 'Sage failed OAQ ' . implode(' ', $data_false));
+
+      }
+    
+    }
+
+    if (isset($_POST['action']) && ($_POST['action'] == 'trans')) {
+
+      $data_false = [];
+
+      foreach ($_POST['target'] as $post_target_id) {
+      
+        $data_sql  = ['category'  =>  $post_target_id];
+        $where_sql = ['id[=]'   =>  $post_target_id]
+        $data = $database->update('content', $data_sql, $where_sql);
+
+        if ($date == false) {
+            
+            $data_false += $post_target_id;
+        
+        }
+      }
+
+      if (count($data_false) == 0) {
+        
+        response_message(200, 'Autobots, Transform and Roll Out!');
+
+      }else {
+
+        response_message(403, 'Trans failed OAQ ' . implode(' ', $data_false));
+
+      }
+    }
+
+    if (isset($_POST['action']) && ($_POST['action'] == 'hummer')) {
+
+      $data_sql  = ['hummer'  =>  $_POST['hummer']];
+      $where_sql = ['id[=]' =>  $_POST['target']];
+      $data = $database->update('content', $data_sql, $where_sql);
+
       if ($data == false) {
-        echo $post_target_id;
+        
+        response_message(403, 'Hummer failed OAQ ');
+      
+      }else {
+
+        response_message(200, '炸鸡馒头');
       }
+    
     }
-    if ($data == true) {
-      response_message(200,'Delet Success!');
-    }else{
-      response_message(403,'Delet Failed!');
-    }
+
+
   }
 
-  if (isset($_POST['action']) && ($_POST['action'] == 'sage')) {
-    foreach ($_POST['target'] as $post_target) {
-      $data = $database->update('content', [
-        'sage'  =>  1
-      ],[
-        'id[=]'    =>  $post_target
-      ]);
-      if ($date == false) {
-        echo $post_target;
-      }
-    }
-    if ($data  == false) {
-      response_message(403, 'Sage Failed!');
-    }else {
-      response_message(200, 'Sage Success!');
-    }
+  function response_message($Code,$Message) {
+
+    $Response = [
+
+      'code'    =>  $Code,
+      'message' =>  $Message
+    
+    ];
+
+    echo json_encode($Response);
+
+    exit();
+  
   }
-
-  if (isset($_POST['action']) && ($_POST['action'] == 'trans')) {
-    foreach ($_POST['target'] as $post_target) {
-      $data = $database->update('content', [
-        'category'  =>  $_POST['target_cate']
-      ],[
-        'id[=]'        =>  $post_target
-      ]);
-      if ($data == false) {
-        echo $post_target;
-      }
-    }
-    if ($data == false) {
-      response_message(403, "Die, Autobots!");
-    }else {
-      response_message(200, "Autobots, Transform and Roll Out.");
-    }
-  }
-
-  if (isset($_POST['action']) && ($_POST['action'] == 'hummer')) {
-    $data = $database->update('content', [
-        'hummer' => $_POST['content']
-      ]);
-    if ($data == false) {
-      response_message(403, 'UCCU');
-    }else {
-      response_message(200, '炸鸡馒头');
-    }
-  }
-}
-
-function response_message($code,$message) {
-  $response = [
-    'code'    =>  $code,
-    'message' =>  $message
-  ];
-
-  echo json_encode($response);
-
-  exit();
-}

871ead2923f7e683aeca9c39bc135cd29e992ca8
Losses Don <losses.don@gmail.com>
2015-02-22 22:27:49 +0800

    删除config文件

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-	
-	define('DB_HOST', 'localhost');
-	define('DB_USER', 'root');
-	define('DB_PASSWORD', 'root');
-	define('DB_NAME', 'gun');
-	define('DB_PORT', '3306');
-	define('UR_PASSWORD', 'LOSSSKY');
-	define('UR_SALT', 'bedfrontniconiconi');

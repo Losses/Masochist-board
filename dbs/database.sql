71f39d1627bda5f9c7d5c5ae279872b35d7d982c
Losses Don <losses.don@gmail.com>
2015-02-22 16:57:13 +0800

    数据库结构更新

--- dbs/database.sql
+++ dbs/database.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.3.9
+-- version 4.2.11
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: 2015-02-17 11:24:40
--- 服务器版本： 10.0.16-MariaDB-log
--- PHP Version: 5.6.5
+-- Host: 127.0.0.1
+-- Generation Time: Feb 22, 2015 at 09:54 AM
+-- Server version: 5.6.21
+-- PHP Version: 5.6.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -23,34 +23,37 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- 表的结构 `category`
+-- Table structure for table `category`
 --
 
 CREATE TABLE IF NOT EXISTS `category` (
-  `id` int(11) NOT NULL,
+`id` int(11) NOT NULL,
   `name` varchar(8) COLLATE utf8_unicode_ci NOT NULL,
+  `mute` tinyint(4) NOT NULL DEFAULT '0',
+  `hide` tinyint(4) NOT NULL DEFAULT '0',
   `theme` varchar(8) COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- 转存表中的数据 `category`
+-- Dumping data for table `category`
 --
 
-INSERT INTO `category` (`id`, `name`, `theme`) VALUES
-('1', '兄贵', 'red'),
-('2', '卖萌', 'orange'),
-('3', '搞基', 'pink'),
-('4', '天空', 'green'),
-('5', '百合', 'blue');
+INSERT INTO `category` (`id`, `name`, `mute`, `hide`, `theme`) VALUES
+(1, '兄贵', 0, 0, 'red'),
+(2, '卖萌', 0, 0, 'orange'),
+(3, '搞基', 0, 0, 'pink'),
+(4, '天空', 0, 0, 'green'),
+(5, '百合', 0, 0, 'blue'),
+(6, '种子', 1, 1, 'teal');
 
 -- --------------------------------------------------------
 
 --
--- 表的结构 `content`
+-- Table structure for table `content`
 --
 
 CREATE TABLE IF NOT EXISTS `content` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+`id` int(11) NOT NULL,
   `author` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL,
   `title` text COLLATE utf8_unicode_ci,
   `content` text COLLATE utf8_unicode_ci NOT NULL,
@@ -70,13 +73,13 @@ CREATE TABLE IF NOT EXISTS `content` (
 -- Indexes for table `category`
 --
 ALTER TABLE `category`
-  ADD PRIMARY KEY (`id`);
+ ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `content`
 --
 ALTER TABLE `content`
-  ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `id_UNIQUE` (`id`), ADD FULLTEXT KEY `index_content` (`title`,`content`);
+ ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `id_UNIQUE` (`id`), ADD FULLTEXT KEY `index_content` (`title`,`content`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -86,12 +89,12 @@ ALTER TABLE `content`
 -- AUTO_INCREMENT for table `category`
 --
 ALTER TABLE `category`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT for table `content`
 --
 ALTER TABLE `content`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

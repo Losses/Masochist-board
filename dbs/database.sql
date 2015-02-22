ffb52986521c43ee8ce32ff8b180520d30830d18
Losses Don <losses.don@gmail.com>
2015-02-22 20:24:12 +0800

    sql更新

--- dbs/database.sql
+++ dbs/database.sql
@@ -1,30 +1,6 @@
--- phpMyAdmin SQL Dump
--- version 4.2.11
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: Feb 22, 2015 at 09:54 AM
--- Server version: 5.6.21
--- PHP Version: 5.6.3
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Database: `gun`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `category`
---
 
 CREATE TABLE IF NOT EXISTS `category` (
 `id` int(11) NOT NULL,
@@ -34,10 +10,6 @@ CREATE TABLE IF NOT EXISTS `category` (
   `theme` varchar(8) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
---
--- Dumping data for table `category`
---
-
 INSERT INTO `category` (`id`, `name`, `mute`, `hide`, `theme`) VALUES
 (1, '兄贵', 0, 0, 'red'),
 (2, '卖萌', 0, 0, 'orange'),
@@ -45,12 +17,6 @@ INSERT INTO `category` (`id`, `name`, `mute`, `hide`, `theme`) VALUES
 (4, '天空', 0, 0, 'green'),
 (5, '百合', 0, 0, 'blue'),
 (6, '种子', 1, 1, 'teal');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `content`
---
 
 CREATE TABLE IF NOT EXISTS `content` (
 `id` int(11) NOT NULL,
@@ -65,36 +31,14 @@ CREATE TABLE IF NOT EXISTS `content` (
   `category` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `category`
---
 ALTER TABLE `category`
  ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `content`
---
 ALTER TABLE `content`
  ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `id_UNIQUE` (`id`), ADD FULLTEXT KEY `index_content` (`title`,`content`);
 
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `category`
---
 ALTER TABLE `category`
 MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
---
--- AUTO_INCREMENT for table `content`
---
+
 ALTER TABLE `content`
 MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

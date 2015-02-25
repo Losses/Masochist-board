6e560e95dbfb0e0f51293fcaafb6d86f9bbf3024
Losses Don <losses.don@gmail.com>
2015-02-25 16:53:47 +0800

    debug

--- scripts/public.js
+++ scripts/public.js
@@ -48,9 +48,35 @@ mKnowledge.filter('trustHtml', function ($sce) {
 });
 
 //进入全屏模式
-/mobile/i.test(navigator.userAgent) && !pageYOffset && !location.hash && setTimeout(function () {
-    window.scrollTo(0, 1);
-}, 1000);​
+(function (win) {
+    var doc = win.document;
+
+    if (!location.hash && win.addEventListener) {
+
+        window.scrollTo(0, 1);
+        var scrollTop = 1,
+            getScrollTop = function () {
+                return win.pageYOffset || doc.compatMode === "CSS1Compat" && doc.documentElement.scrollTop || doc.body.scrollTop || 0;
+            },
+
+            bodycheck = setInterval(function () {
+                if (doc.body) {
+                    clearInterval(bodycheck);
+                    scrollTop = getScrollTop();
+                    win.scrollTo(0, scrollTop === 1 ? 0 : 1);
+                }
+            }, 15);
+        win.addEventListener("load", function () {
+            setTimeout(function () {
+
+                if (getScrollTop() < 20) {
+
+                    win.scrollTo(0, scrollTop === 1 ? 0 : 1);
+                }
+            }, 0);
+        });
+    }
+})(this);
 
 var losses = {router: {}, scope: {}, data: {}, global: {}, multiSelect: []};
 

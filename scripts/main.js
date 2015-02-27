9270a58261753042c596e5424592889913038d6a
Losses Don <losses.don@gmail.com>
2015-02-27 09:42:10 +0800

    顶部导航栏针对触屏进行优化
    当点击顶部导航栏1500ms之后高光会退出屏幕

--- scripts/main.js
+++ scripts/main.js
@@ -3,12 +3,25 @@
  */
 
 (function () {
-    var headerList = $('header>nav') /*nxt line*/
-        , highlightItem = document.querySelector('.highlight');
+    var bodyElement = $('body') /*nxt line*/
+        , headerList = $('header>nav') /*nxt line*/
+        , highlightItem = document.querySelector('.highlight') /*nxt line*/
+        , timeoutEvent = false;
 
     headerList.delegate('li', 'mouseenter', function () {
+        var that = this;
+        if (timeoutEvent)
+            clearTimeout(timeoutEvent);
+
         $(highlightItem).css('left', $(this).position().left);
         $(this).addClass('high');
+
+        if (bodyElement.hasClass('touch_screen')) {
+            timeoutEvent = setTimeout(function () {
+                $(that).removeClass('high');
+                highlightItem.setAttribute('style', 'left:1000px');
+            }, 1500);
+        }
     }).delegate('li', 'mouseleave', function () {
         $(this).removeClass('high');
     });

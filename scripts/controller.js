fd597a2679a8948a65ff6b971f1d9c4e2cc22ecf
Losses Don <losses.don@gmail.com>
2015-03-04 17:11:40 +0800

    重新生成css文件

--- scripts/controller.js
+++ scripts/controller.js
@@ -146,9 +146,8 @@ function manageStarter($scope, $http) {
             data: $.param({'system_info': ''}),
             headers: {'Content-Type': 'application/x-www-form-urlencoded'}
         }).success(function (response) {
-            console.log(response);
             $scope.systemInfo = response;
-        })
+        });
     }
 
     if ($('#masochist-manage-style')[0])
@@ -165,6 +164,24 @@ function manageStarter($scope, $http) {
     StyleFix.styleElement(newStyleSheet[0]);
 
     $scope.logout = function () {
-        console.log('!');
+        $http({
+            method: 'POST',
+            url: 'api/?manage',
+            data: $.param({'action': 'logout'}),
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+        }).success(function (response) {
+            if (response.code) {
+                losses.global.logined = false;
+                losses.global.$digest();
+            }
+
+            if (response.code == 200) {
+                magicalLocation('#/');
+            } else if (response.message) {
+                publicWarning(response.message);
+            } else {
+                publicWarning(response);
+            }
+        })
     }
 }

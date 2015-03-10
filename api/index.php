4060d289c70cc5c4397709ec31aff34f286e1c77
Losses Don <losses.don@gmail.com>
2015-03-10 18:52:38 +0800

    api format

--- api/index.php
+++ api/index.php
@@ -722,7 +722,7 @@ function masochist_debug($info)
 
     if(is_array($info))
         $info = json_encode($info);
-    
+
     $write_str = "[$date] $info \n";
     file_put_contents('../dbs/debug_info', $write_str, FILE_APPEND);
 }
